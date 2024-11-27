76429daa0707a874f3dd6a0eda4745bb7979dc02
jonathanwu <jonathan0127lhps@gmail.com>
2024-11-27 22:48:32 +0800

    hw7

--- HW/Assignment 7/hw7-B123040048.cpp
+++ HW/Assignment 7/hw7-B123040048.cpp
@@ -46,7 +46,46 @@ void selection(vector<int> &vt) {
 	}
 }
 
+void adjust(vector<int> &vt, int last, int cur) {
+	int l = 2 * cur + 1;
+	int r = 2 * cur + 2;
 
+	int large;
+
+	if (l <= last && vt[l] > vt[cur]) {
+		large = l;
+	}
+	else {
+		large = cur;
+	}
+
+	if (r <= last && vt[r] > vt[large]) {
+		large = r;
+	}
+
+	if (large != cur) {
+		swap(vt[cur], vt[large]);
+		adjust(vt, last, large);
+	}
+}
+
+
+void heap(vector<int> &vt) {
+	for (int i = vt.size() / 2 - 1 ; i >= 0 ; i --) {
+		adjust(vt, vt.size() - 1, i);
+	}
+
+	for (int i = vt.size() - 1 ; i > 0 ; i --) {
+		swap(vt[0], vt[i]);
+		adjust(vt, i - 1, 0);
+	}
+
+}
+
+
+void Quick(vector<int> &vt, int l, int r) {
+
+}
 
 
 
@@ -57,6 +96,7 @@ signed main() {
 	vector<int> f_input;
 	outa.open("outputA.txt");
 	outb.open("outputB.txt");
+	outc.open("outputC.txt");
 
 	cout << "1. sort the input.txt\n2. sort the random data\n";
 	cin >> op;
@@ -73,6 +113,13 @@ signed main() {
 		heap(f_input);
 		output(outb, f_input, "Heap Sort");
 		cout << "Finish!\n";
+
+		read(f_input);
+		cout << "Quick sort......";
+		Quick(f_input, 0, f_input.size() - 1);
+		output(outc, f_input, "Quick Sort");
+		cout << "Finish!\n";
+
 	}
 	else if (op == 2) {
 
